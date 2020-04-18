d891b444d82b5fe21b2233cd8b5e20025dc65ee4
Noel Arroyo <noel.arroyo1@gmail.com>
2020-04-18 17:38:34 -0400

    add more functionality to beat maker app

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 class BeatMachine {
   constructor() {
     this.pads = document.querySelectorAll(".pad");
+    this.currentKick = "./sounds/kick-classic.wav";
+    this.currentSnare = "./sounds/snare-acoustic01.wav";
+    this.currentHihat = "./sounds/hihat-acoustic01.wav";
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
     this.bpm = 160;
     this.playBtn = document.querySelector(".play");
+    this.muteBtns = document.querySelectorAll(".mute");
+    this.tempoSlider = document.querySelector(".tempo-slider");
     this.isPlaying = null;
+    this.selects = document.querySelectorAll("select");
   }
+
   repeat() {
     let beat = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${beat}`);
@@ -33,6 +40,7 @@ class BeatMachine {
     });
     this.index++;
   }
+
   start() {
     const interval = (60 / this.bpm) * 1000;
     if (!this.isPlaying) {
@@ -44,12 +52,85 @@ class BeatMachine {
       this.isPlaying = null;
     }
   }
+
+  updateBtn() {
+    if (this.isPlaying) {
+      this.playBtn.innerText = "Stop";
+      this.playBtn.classList.add("active");
+    } else {
+      this.playBtn.innerText = "Play";
+      this.playBtn.classList.remove("active");
+    }
+  }
+
   activePad() {
     this.classList.toggle("active");
+  }
+
+  changeSound(e) {
+    const selectionName = e.target.name;
+    const selectionValue = e.target.value;
+    switch (selectionName) {
+      case "kick-select":
+        this.kickAudio.src = selectionValue;
+        break;
+      case "snare-select":
+        this.snareAudio.src = selectionValue;
+        break;
+      case "hihat-select":
+        this.hihatAudio.src = selectionValue;
+        break;
+    }
+  }
+
+  muteSound(e) {
+    const muteIndex = e.target.getAttribute("data-track");
+    e.target.classList.toggle("active");
+    if (e.target.classList.contains("active")) {
+      switch (muteIndex) {
+        case "0":
+          this.kickAudio.volume = 0;
+          break;
+        case "1":
+          this.snareAudio.volume = 0;
+          break;
+        case "2":
+          this.hihatAudio.volume = 0;
+          break;
+      }
+    } else {
+      switch (muteIndex) {
+        case "0":
+          this.kickAudio.volume = 1;
+          break;
+        case "1":
+          this.snareAudio.volume = 1;
+          break;
+        case "2":
+          this.hihatAudio.volume = 1;
+          break;
+      }
+    }
+  }
+
+  changeTempo(e) {
+    const tempoText = document.querySelector(".tempo-number");
+    this.bpm = e.target.value;
+    tempoText.innerText = e.target.value;
+  }
+
+  updateTempo() {
+    clearInterval(this.isPlaying);
+    this.isPlaying = null;
+    if (this.playBtn.classList.contains("active")) {
+      this.start();
+    }
   }
 }
 
 const beatMachine = new BeatMachine();
+
+//Event Listeners
 
 beatMachine.pads.forEach((pad) => {
   pad.addEventListener("click", beatMachine.activePad);
@@ -60,9 +141,25 @@ beatMachine.pads.forEach((pad) => {
 
 beatMachine.playBtn.addEventListener("click", function () {
   beatMachine.start();
-  if (this.innerText === "Play") {
-    this.innerText = "Stop";
-  } else {
-    this.innerText = "Play";
-  }
+  beatMachine.updateBtn();
+});
+
+beatMachine.selects.forEach((select) => {
+  select.addEventListener("change", function (e) {
+    beatMachine.changeSound(e);
+  });
+});
+
+beatMachine.muteBtns.forEach((mute) => {
+  mute.addEventListener("click", function (e) {
+    beatMachine.muteSound(e);
+  });
+});
+
+beatMachine.tempoSlider.addEventListener("input", function (e) {
+  beatMachine.changeTempo(e);
+});
+
+beatMachine.tempoSlider.addEventListener("change", function (e) {
+  beatMachine.updateTempo(e);
 });
